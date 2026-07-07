946ec74e6e5fc966ed0c007020a47f9cc4a7322c
Mario <mapa.dev.tec1@gmail.com>
2026-07-07 13:55:22 -0300

    Solução do primeiro exercicio (finalizado)

--- 1Fundamentos/1_1Fundamentos_de_Sintaxe/Exercicio1.java
+++ 1Fundamentos/1_1Fundamentos_de_Sintaxe/Exercicio1.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
+//escreva um programa que receba o noma e o ano de nascimento de alguém
+//e imprima na tela a seguinte mensagem: 
+//"Olá, Fulano. você tem X anos!"
+
 public class Exercicio1 {
-    
+    public static void main(String[] args) {
+        String nome;
+        int idade;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        nome = in.nextLine();
+
+        System.out.println("Digite sua idade: ");
+        idade = in.nextInt();
+
+        System.out.println
+        ("Olá, " + nome + ". Você tem " + idade + " anos!");
+
+        in.close();
+    }
 }
