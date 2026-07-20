2bddef961e8a08d9ac20a564d6a461497db33da5
Mario <mapa.dev.tec1@gmail.com>
2026-07-20 16:40:35 -0300

    inclusao secao 1_2 exerc2

--- 1Fundamentos/1_2Estrututas_de_controle/Exercicio2.java
+++ 1Fundamentos/1_2Estrututas_de_controle/Exercicio2.java
@@ -1,1 +1,26 @@
-//crie um programa que calcula o imc do usuario
+import java.util.Scanner;
+
+//crie um programa que calcula o imc do usuario -> peso / (alt*alt)
+
+public class Exercicio2{
+    public static void main(String[] args) {
+        double peso;
+        double altura;
+        double imc;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("*** Calculadora de IMC ***");
+
+        System.out.println("Digite seu peso: ");
+        peso = in.nextDouble();
+
+        System.out.println("Digite sua altura: ");
+        altura = in.nextDouble();
+
+        imc = peso / (altura*altura);
+
+        System.out.println("Seu IMC é: " + imc);
+        in.close();
+    }
+}
